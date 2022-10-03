cec8895a8865d458517c4276457a65461e04cdbf
Hamza Kermadi <hakermad@MacBook-Pro-de-Hamza.local>
2022-10-03 12:50:35 +0200

    Testing image uploader

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@ import App from "./App.js";
 
 const root = document.getElementById("app");
 const app = new App(root);
+
+const image_input = document.querySelector("#image-input");
+image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
